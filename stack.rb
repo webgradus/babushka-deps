ba7f720eaed036f4ca80659f20f3416ddc3faae2
PitOn <garik.piton@gmail.com>
2012-11-29 12:37:00 +0400

    removed git - it's installed by babushka

--- stack.rb
+++ stack.rb
@@ -1,3 +1,3 @@
 dep 'stack' do
-  requires 'git.managed', 'running.nginx', 'mysql configured', 'postgres.managed', 'imagemagick.managed', 'rvm'
+  requires 'running.nginx', 'mysql configured', 'postgres.managed', 'imagemagick.managed', 'rvm'
 end
