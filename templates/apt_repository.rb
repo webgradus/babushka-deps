cc2e7e23ce3b260654d91ac955eec215e6cef123
PitOn <garik.piton@gmail.com>
2012-11-29 15:08:52 +0400

    Update templates/apt_repository.rb

--- templates/apt_repository.rb
+++ templates/apt_repository.rb
@@ -8,7 +8,7 @@ meta :apt_repository do
   end
 
   def deb
-    "deb #{url} #{distribution.blank? ? System.codename : distribution} #{components.join(' ')}"
+    "deb #{url} #{distribution.blank? ? SystemDetector.profile_for_host.name : distribution} #{components.join(' ')}"
   end
 
   template do
