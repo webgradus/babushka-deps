544238b4d6a49a90e68c6053cec2340f0992c978
PitOn <garik.piton@gmail.com>
2012-11-29 13:38:20 +0400

    mongo added to stack

--- stack.rb
+++ stack.rb
@@ -1,3 +1,3 @@
 dep 'stack' do
-  requires 'running.nginx', 'mysql configured', 'postgres.managed', 'imagemagick.managed', 'rvm'
+  requires 'running.nginx', 'mysql configured', 'postgres.managed', 'latest mongo', 'imagemagick.managed', 'rvm'
 end
