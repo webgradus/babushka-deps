1f6d9de8c691f2ba90a1247434be4a5f77b0becb
PitOn <garik.piton@gmail.com>
2012-11-29 12:27:54 +0400

    imagemagick added

--- stack.rb
+++ stack.rb
@@ -1,3 +1,3 @@
 dep 'stack' do
-  requires 'git.managed', 'running.nginx', 'mysql configured', 'postgres.managed', 'rvm'
+  requires 'git.managed', 'running.nginx', 'mysql configured', 'postgres.managed', 'imagemagick.managed', 'rvm'
 end
