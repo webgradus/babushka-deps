4ae03cb2f2be3c7576ba8531cf0aa8f5cc328a49
PitOn <garik.piton@gmail.com>
2012-11-30 11:44:58 +0400

    Update helpers/rvm.rb

--- helpers/rvm.rb
+++ helpers/rvm.rb
@@ -7,7 +7,7 @@ def rvm_installed?
 end
 
 def rvm_run cmd    
-    log_shell("rvm_run: #{cmd}", rvm_script + cmd)
+    log_shell("rvm_run: #{cmd}", rvm_script + "sudo " + cmd)
 end
 
 def current_rubies
