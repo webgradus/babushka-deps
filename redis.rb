e44171f19c1e5cd461cb7697fc8f42b42dbbdbf4
PitOn <garik.piton@gmail.com>
2012-12-27 17:09:33 +0400

    Update redis.rb

--- redis.rb
+++ redis.rb
@@ -1,2 +1,21 @@
-dep "redis"
+dep 'redis.src', :version do  
+  version.default!('2.6.7')  
+  source "http://redis.googlecode.com/files/redis-#{version}.tar.gz"    
+  prefix '/opt/redis'
+  provides prefix / 'src/redis-server'
+
+  configure { log_shell "configure", "" }
+  build { log_shell "build", "make" }
+  install {  }
+
+  met? {
+    if !File.executable?(prefix / 'src/redis-server')
+      log "redis isn't installed"
+    else
+      installed_version = shell(prefix / 'src/redis-server -v') {|shell| shell.stderr }.val_for(/Redis server version/).sub('(00000000:0)', '')
+      (installed_version == version).tap {|result|
+        log "redis-#{installed_version} is installed"
+      }
+    end
+  }
 end
