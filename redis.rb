7ecf32a0c73486c6f3436307ab410fe4fd4656d1
PitOn <garik.piton@gmail.com>
2012-12-27 17:36:49 +0400

    :path default value

--- redis.rb
+++ redis.rb
@@ -1,7 +1,7 @@
-dep 'redis.src', :version do  
+dep 'redis.src', :version, :path do  
   version.default!('2.6.7')  
   source "http://redis.googlecode.com/files/redis-#{version}.tar.gz"
-  path "/opt/redis-#{version}"
+  path.default!("/opt/redis-#{version}")
   
   met? { (path / "src/redis-server").exists? && File.executable?(path / "src/redis-server")}
   meet {
