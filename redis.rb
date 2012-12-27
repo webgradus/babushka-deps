9620f7913e8226c5c01a18a2105d3c62ae0919e3
PitOn <garik.piton@gmail.com>
2012-12-27 17:44:05 +0400

    correct source variable

--- redis.rb
+++ redis.rb
@@ -1,6 +1,6 @@
 dep 'redis installed', :version, :path do  
   version.default!('2.6.7')  
-  source "http://redis.googlecode.com/files/redis-#{version}.tar.gz"
+  source = "http://redis.googlecode.com/files/redis-#{version}.tar.gz"
   path.default!("/opt/redis-#{version}")
   
   met? { (path / "src/redis-server").exists? && File.executable?(path / "src/redis-server")}
