7de58deff52adf5b4527e4728562459db6a62634
PitOn <garik.piton@gmail.com>
2012-12-27 18:14:30 +0400

    added redis init script

--- redis.rb
+++ redis.rb
@@ -1,3 +1,8 @@
+dep 'redis', :version, :path do
+  requires 'redis installed'.with(version, path)
+  requires 'redis-init-script'
+end
+
 dep 'redis installed', :version, :path do  
   version.default!('2.6.7')  
   source = "http://redis.googlecode.com/files/redis-#{version}.tar.gz"
@@ -11,4 +16,24 @@ dep 'redis installed', :version, :path do
       shell "rm redis-#{version}.tar.gz", :cd => "/opt"
   }
   
+end
+
+dep 'redis-init-script copied' do  
+  met? {    
+    Babushka::Renderable.new("/etc/init.d/redis").from?(dependency.load_path.parent / "init/init_redis.sh.erb")
+  }
+  meet {    
+    render_erb "init/init_redis.sh.erb", :to => "/etc/init.d/redis", :perms => '755', :sudo => true    
+  }
+  
+end
+
+dep 'redis-init-script' do  
+  requires 'redis-init-script copied'
+  requires 'rcconf.managed'
+  
+  met? { shell("rcconf --list").val_for('redis') == 'on' }
+  meet {
+    sudo "update-rc.d redis defaults"
+  }
 end
