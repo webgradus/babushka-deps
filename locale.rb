e499185cd9d75aed63c86e48fc34ab738157f178
PitOn <garik.piton@gmail.com>
2013-03-29 13:02:00 +0400

    Update locale.rb

--- locale.rb
+++ locale.rb
@@ -12,9 +12,9 @@ end
 dep 'set.locale', :locale_name do
   locale_name.default!('en_US')
   requires 'exists.locale'.with(locale_name)
-  met? {
+  #met? {
     #shell('locale').val_for('LANG')[locale_regex(locale_name)]
-  }
+  #}
   on :apt do
     meet {
       sudo("echo 'LANG=#{local_locale(locale_name)}' > /etc/default/locale")
