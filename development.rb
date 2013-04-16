c01c75b65617658db18f2725d54adfa91d588b17
PitOn <garik.piton@gmail.com>
2013-04-16 17:01:50 +0400

    using app_path as path prefix for deploy preparing

--- development.rb
+++ development.rb
@@ -1,12 +1,13 @@
-dep 'prepare-deploy', :app_name, :git_username, :server_ip do
+dep 'prepare-deploy', :app_name, :git_username, :server_ip, :app_path do
   app_name.ask("What is the name of application that will be located at /opt")
   git_username.ask('What is your Git username (login) on GitEnterprise')
   server_ip.ask('Where to deploy - I need IP')
+  app_path.default("")
   met? {
     Babushka::Renderable.new("config/deploy.rb").from?(dependency.load_path.parent / "development/deploy.rb.erb") && Babushka::Renderable.new("config/unicorn.rb").from?(dependency.load_path.parent / "development/unicorn.rb.erb")
   }
   meet {
-    render_erb "development/deploy.rb.erb", :to => "config/deploy.rb"
-    render_erb "development/unicorn.rb.erb", :to => "config/unicorn.rb"
+    render_erb "development/deploy.rb.erb", :to => (app_path / "config/deploy.rb").to_s
+    render_erb "development/unicorn.rb.erb", :to => (app_path / "config/unicorn.rb").to_s
   }
 end
