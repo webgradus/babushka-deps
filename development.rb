390bfec0853a38b751ea56384232da24f5ff0c67
PitOn <garik.piton@gmail.com>
2013-04-16 17:16:48 +0400

    fixes for app_path prefix

--- development.rb
+++ development.rb
@@ -4,7 +4,7 @@ dep 'prepare-deploy', :app_name, :git_username, :server_ip, :app_path do
   server_ip.ask('Where to deploy - I need IP')
   app_path ||= ""
   met? {
-    Babushka::Renderable.new("config/deploy.rb").from?(dependency.load_path.parent / "development/deploy.rb.erb") && Babushka::Renderable.new("config/unicorn.rb").from?(dependency.load_path.parent / "development/unicorn.rb.erb")
+    Babushka::Renderable.new((app_path / "config/deploy.rb").to_s).from?(dependency.load_path.parent / "development/deploy.rb.erb") && Babushka::Renderable.new((app_path / "config/unicorn.rb").to_s).from?(dependency.load_path.parent / "development/unicorn.rb.erb")
   }
   meet {
     render_erb "development/deploy.rb.erb", :to => (app_path / "config/deploy.rb").to_s
