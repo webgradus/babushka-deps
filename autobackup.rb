043e12d2928c52a2adffc367c5dd3adb6eea1665
Alexandr Nigomatulin <nigomatulin@gmail.com>
2013-05-06 16:47:41 +0400

    AutoBackup

--- autobackup.rb
+++ autobackup.rb
@@ -1,27 +1,29 @@
 dep 'autobackup', :app_name, :app_path, :ruby_version do
-  app_name.ask("What is the name of application that will be located at /opt")
+  requires 'schedule'
+  app_name.ask("What is the name of application?")
   app_path.default!("")
-  ruby_version.default("2.0.0").choose(%w[2.0.0 1.9.3])
+
+  if !(app_path / "config/backup").exists?
+    shell %{ mkdir "config/backup"}
+    shell %{ mkdir "config/backup/models"}
+  end
+
   met? {
-    Babushka::Renderable.new(app_path / "config/backup/config.rb").from?(dependency.load_path.parent / "autobackup/config.rb.erb") && Babushka::Renderable.new(app_path / "config/backup/models/webgradus-backup.rb").from?(dependency.load_path.parent / "autobackup/webgradus-backup.rb.erb")
+    Babushka::Renderable.new(app_path / "config/backup/config.rb").from?(dependency.load_path.parent / "autobackup/config.rb.erb") && Babushka::Renderable.new(app_path / "config/backup/models/webgradus_backup.rb").from?(dependency.load_path.parent / "autobackup/webgradus_backup.rb.erb")
   }
   meet {
     render_erb "autobackup/config.rb.erb", :to => (app_path / "config/backup/config.rb").to_s
-    render_erb "autobackup/webgradus-backup.rb.erb", :to => (app_path / "config/backup/models/webgradus-backup.rb").to_s
-    
+    render_erb "autobackup/webgradus_backup.rb.erb", :to => (app_path / "config/backup/models/webgradus_backup.rb").to_s 
   }
   
-   if (app_path / "config/schedule.rb").exists?
-    shell %{echo 'job_type :backup, "cd :path && rvm use #{ruby_version} do bundle exec backup perform -t general -c config/backup/config.rb"' >> config/schedule.rb}
-    shell %{ echo 'every 3.days do' >> config/schedule.rb}
-    shell %{ echo 'backup ""' >> config/schedule.rb}
-    shell %{ echo 'end' >> config/schedule.rb}
-   else
-    met? {
-	Babushka::Renderable.new(app_path / "config/schedule.rb").from?(dependency.load_path.parent / "autobackup/schedule.rb.erb")
-    }
-    meet {
-	render_erb "autobackup/schedule.rb.erb", :to => (app_path / "config/schedule.rb").to_s
-    }
-   end
+
+end
+
+dep 'schedule', :app_path, :ruby_version do
+  app_path.default!("")
+  ruby_version.default("2.0.0").choose(%w[2.0.0 1.9.3])
+  shell %{echo 'job_type :backup, "cd :path && rvm use #{ruby_version} do bundle exec backup perform -t general -c config/backup/config.rb"' >> config/schedule.rb}
+  shell %{ echo 'every 3.days do' >> config/schedule.rb}
+  shell %{ echo 'backup ""' >> config/schedule.rb}
+  shell %{ echo 'end' >> config/schedule.rb}
 end
