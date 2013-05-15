b8a1b959213e64b32396e65b97b076f5e70df81b
Alexandr Nigomatulin <nigomatulin@gmail.com>
2013-05-15 17:31:16 +0400

    backup

--- autobackup.rb
+++ autobackup.rb
@@ -9,30 +9,21 @@ dep 'autobackup', :app_name, :app_path, :ruby_version do
   end
 
   met? {
-    Babushka::Renderable.new(app_path / "config/backup/config.rb").from?(dependency.load_path.parent / "autobackup/config.rb.erb") && Babushka::Renderable.new(app_path / "config/backup/models/webgradus_backup.rb").from?(dependency.load_path.parent / "autobackup/webgradus_backup.rb.erb") && Babushka::Renderable.new(app_path / "config/backup.yml").from?(dependency.load_path.parent / "autobackup/backup.yml")
+    Babushka::Renderable.new(app_path / "config/backup/config.rb").from?(dependency.load_path.parent / "autobackup/config.rb.erb") && Babushka::Renderable.new(app_path / "config/backup/models/webgradus_backup.rb").from?(dependency.load_path.parent / "autobackup/webgradus_backup.rb.erb") && Babushka::Renderable.new(app_path / "config/backup.yml").from?(dependency.load_path.parent / "autobackup/backup.yml.erb")
   }
   meet {
     render_erb "autobackup/config.rb.erb", :to => (app_path / "config/backup/config.rb").to_s
     render_erb "autobackup/webgradus_backup.rb.erb", :to => (app_path / "config/backup/models/webgradus_backup.rb").to_s
-    render_erb "autobackup/backup.yml", :to => (app_path "config/backup.yml")
+    render_erb "autobackup/backup.yml.erb", :to => (app_path / "config/backup.yml").to_s
     shell "echo 'group :backup do' >> Gemfile"
-    shell %{echo 'gem "backup", "3.3.2"' >> Gemfile}
-    shell %{echo 'gem "whenever"' >> Gemfile}
-    shell %{echo 'gem "fog", "~>1.9.0"' >> Gemfile}
-    shell %{echo 'gem "net-ssh", "<= 2.5.2"' >> Gemfile}
-    shell %{echo 'gem "mail", "~>2.5.0"' >> Gemfile}
-    shell %{echo 'gem "excon", "~>0.17.0"' >> Gemfile}
+    shell %{echo ' gem "backup", "3.3.2"' >> Gemfile}
+    shell %{echo ' gem "whenever"' >> Gemfile}
+    shell %{echo ' gem "fog", "~>1.9.0"' >> Gemfile}
+    shell %{echo ' gem "net-ssh", "<= 2.5.2"' >> Gemfile}
+    shell %{echo ' gem "mail", "~>2.5.0"' >> Gemfile}
+    shell %{echo ' gem "excon", "~>0.17.0"' >> Gemfile}
     shell "echo 'end' >> Gemfile"
   }
-
-  met? {
-      "config/backup.yml".p.exists?
-  }
-  meet
-  {
-      shell "cp #{dependency.load_path.parent / }autobackup/backup.yml #{app_path}/config"
-  }
-  
 
 end
 
