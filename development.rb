404a8c0145efeb0162dc35790c5e8db4a4514961
PitOn <garik.piton@gmail.com>
2013-05-27 14:51:51 +0400

    require 'autobackup' on deploy prepare

--- development.rb
+++ development.rb
@@ -1,9 +1,11 @@
-dep 'prepare-deploy', :app_name, :git_username, :server_ip, :app_path, :app_type do
+dep 'prepare-deploy', :app_name, :git_username, :server_ip, :app_path, :app_type, :ruby_version do
   app_name.ask("What is the name of application that will be located at /opt")
   git_username.ask('What is your Git username (login) on GitEnterprise')
   server_ip.ask('Where to deploy - I need IP')
   app_type.default('rails').choose(%w[rails locomotive])
   app_path.default!("")
+  ruby_version.default("2.0.0").choose(%w[2.0.0 1.9.3])
+  requires 'autobackup'.with(app_name, app_path, ruby_version)
   met? {
     Babushka::Renderable.new(app_path / "config/deploy.rb").from?(dependency.load_path.parent / "development/deploy.rb.erb") && Babushka::Renderable.new(app_path / "config/unicorn.rb").from?(dependency.load_path.parent / "development/unicorn.rb.erb")
   }
