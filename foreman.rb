25197f5d0ab376db1b2992fabf8f8968d90640f0
Михаил Поспелов <mixan946@gmail.com>
2013-06-27 16:26:37 +0400

    foreman deployment

--- foreman.rb
+++ foreman.rb
@@ -1,8 +1,9 @@
-dep 'foreman', :app_path, :use_faye? do
+dep 'foreman', :app_path, :use_faye?, :foreman_in_gemfile? do
   use_faye?.default('no').choose(%w[yes no])
+  foreman_in_gemfile = shell %{grep "foreman" Gemfile}.present?
   met? {
     Babushka::Renderable.new(app_path / "Procfile.production").from?(dependency.load_path.parent / "foreman/Procfile.production.erb") &&
-    shell %{ grep 'foreman' ./Gemfile }
+    foreman_in_gemfile
   }
   meet {
     render_erb "foreman/Procfile.production.erb", :to => (app_path / "Procfile.production").to_s
