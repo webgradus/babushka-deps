3e441f6d75ad0eafb7cfddfbae73c0c1c8a2812d
Михаил Поспелов <mixan946@gmail.com>
2013-06-28 08:37:29 +0400

    foreman deployment

--- development.rb
+++ development.rb
@@ -1,4 +1,4 @@
-dep 'prepare-deploy', :app_name, :git_username, :server_ip, :app_path, :app_type, :ruby_version do
+dep 'prepare-deploy', :app_name, :git_username, :server_ip, :app_path, :app_type, :ruby_version, :faye do
   app_name.ask("What is the name of application that will be located at /opt")
   git_username.ask('What is your Git username (login) on GitEnterprise')
   server_ip.ask('Where to deploy - I need IP')
@@ -6,7 +6,7 @@ dep 'prepare-deploy', :app_name, :git_username, :server_ip, :app_path, :app_type
   app_path.default!("")
   ruby_version.default("2.0.0").choose(%w[2.0.0 1.9.3])
   requires 'autobackup'.with(app_name, app_path, ruby_version)
-  requires 'foreman'.with(app_path, 'no')
+  requires 'foreman'.with(app_path, faye)
   met? {
     Babushka::Renderable.new(app_path / "config/deploy.rb").from?(dependency.load_path.parent / "development/deploy.rb.erb") && Babushka::Renderable.new(app_path / "config/unicorn.rb").from?(dependency.load_path.parent / "development/unicorn.rb.erb")
   }
