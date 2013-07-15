7d8aad4ade3cb67983efcbcceccd53d8b2dfd52c
Alex Nigomatulin <alex4578@yandex.ru>
2013-07-15 10:52:28 +0400

    Update development.rb

--- development.rb
+++ development.rb
@@ -1,12 +1,13 @@
-dep 'prepare-deploy', :app_name, :git_username, :server_ip, :app_path, :app_type, :ruby_version, :use_faye do
+dep 'prepare-deploy', :app_name, :git_username, :server_ip, :app_path, :app_type, :ruby_version, :use_faye, :database do
   app_name.ask("What is the name of application that will be located at /opt")
   git_username.ask('What is your Git username (login) on GitEnterprise')
   server_ip.ask('Where to deploy - I need IP')
   app_type.default('rails').choose(%w[rails locomotive])
+  database.default("mysql").choose(%w[mysql postgresql])
   app_path.default!("")
   ruby_version.default("2.0.0").choose(%w[2.0.0 1.9.3])
   use_faye.default('no').choose(%w[yes no])
-  requires 'autobackup'.with(app_name, app_path, ruby_version)
+  requires 'autobackup'.with(app_name, app_path, ruby_version, database)
   requires 'foreman'.with(app_path, use_faye)
   met? {
     Babushka::Renderable.new(app_path / "config/deploy.rb").from?(dependency.load_path.parent / "development/deploy.rb.erb") && Babushka::Renderable.new(app_path / "config/unicorn.rb").from?(dependency.load_path.parent / "development/unicorn.rb.erb")
