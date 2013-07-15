974501edf4e29f5cfc7456d36403af76d361d245
Alex Nigomatulin <alex4578@yandex.ru>
2013-07-15 11:08:26 +0400

    Update foreman.rb

--- foreman.rb
+++ foreman.rb
@@ -1,8 +1,7 @@
 dep 'foreman', :app_path, :use_faye do
-  foreman_in_gemfile = shell? %{grep "foreman" Gemfile}
   met? {
     Babushka::Renderable.new(app_path / "Procfile.production").from?(dependency.load_path.parent / "foreman/Procfile.production.erb") &&
-    foreman_in_gemfile
+    shell? %{grep "foreman" Gemfile}
   }
   meet {
     render_erb "foreman/Procfile.production.erb", :to => (app_path / "Procfile.production").to_s
