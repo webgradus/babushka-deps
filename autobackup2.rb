39c0e4482d95adfdfc28651897d47713c8ec0178
Alex Nigomatulin <alex4578@yandex.ru>
2013-07-17 13:05:09 +0400

    Update autobackup2.rb

--- autobackup2.rb
+++ autobackup2.rb
@@ -4,19 +4,20 @@ dep 'autobackup2', :app_name, :app_path, :database do
   database.default("mysql").choose(%w[mysql postgresql mongodb])
   
   met? {
-    Babushka::Renderable.new("/root/Backup/models/#{ app_name}.rb").from?(dependency.load_path.parent / "autobackup/model_template.rb.erb")  }
+    Babushka::Renderable.new("/root/Backup/models/#{app_name}.rb").from?(dependency.load_path.parent / "autobackup/model_template.rb.erb")
+  }
   meet {
     render_erb "autobackup/model_tamplate.rb.erb", :to => ("/root/Backup/models/#{app_name}.rb").to_s
   }
 
   met? {
-      shell? %{ grep #{app_name} /root/Backup/config/schedule.rb}
+    shell? %{ grep #{app_name} /root/Backup/config/schedule.rb}
   }
   meet {
-      shell %{ echo 'every 1.week do' >> /root/Backup/config/schedule.rb }
-      shell %{ echo 'command "backup perform -t #{app_name}"' >> /root/Backup/config/schedule.rb }
-      shell %{ echo 'end' >> /root/Backup/config/schedule.rb }
-      shell %{ wheneverize }
+    shell %{ echo 'every 1.week do' >> /root/Backup/config/schedule.rb }
+    shell %{ echo '  command "backup perform -t #{app_name}"' >> /root/Backup/config/schedule.rb }
+    shell %{ echo 'end' >> /root/Backup/config/schedule.rb }
+    shell %{ whenever }
   }
  
 end
