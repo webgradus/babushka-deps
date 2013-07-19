d111bee83b17612f4da34a9142177d61a55750dd
Alexandr Nigomatulin <nigomatulin@gmail.com>
2013-07-19 10:35:11 +0400

    backup install on server

---
+++ backup-server.rb
@@ -0,0 +1,20 @@
+dep 'backup-server' do
+    log "backup gem install..."
+    shell "gem install backup"
+    shell "backup generate:config"
+    shell "mkdir models", :cd => "/root/Backup/"
+
+    log "whenever gem install..."
+    shell "gem install whenever"
+    shell "mkdir config", :cd => "/root/Backup/"
+    shell "wheneverize ."
+
+    log "copy config file..."
+    met? {
+        Babushka::Renderable.new("/root/Backup/config.rb").from?(dependency.load_path.parent / "backup-server/config.rb.erb")
+    }
+    meet {
+        render_erb "backup-server/config.rb.erb", :to => ("/root/Backup/config.rb").to_s
+    }
+    
+end
