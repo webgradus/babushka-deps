537dd1c50b26418262d1b2facc56c032fe327bd5
PitOn <garik.piton@gmail.com>
2013-08-13 17:48:23 +0400

    using block to access stdout

--- rails.rb
+++ rails.rb
@@ -1,5 +1,5 @@
 dep 'rails installed', :ruby_version do
-met? { rvm_run("use #{ruby_version} do gem list rails").stdout.include?("rails (") }
+met? { rvm_run("use #{ruby_version} do gem list rails"){|shell| shell.stdout.include?("rails (") } }
 meet {
   rvm_run "use #{ruby_version} do gem install rails --no-rdoc --no-ri"
 }   
