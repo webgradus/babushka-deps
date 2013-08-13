ccb1e8d61f2df09a8d3c6940561c8b0c646bfd71
PitOn <garik.piton@gmail.com>
2013-08-13 17:13:19 +0400

    change rails dep name

--- rails.rb
+++ rails.rb
@@ -1,4 +1,4 @@
-dep 'rails.installed', :ruby_version do
+dep 'rails installed', :ruby_version do
 met? { raw_shell("rvm use #{ruby_version} do gem list rails").stdout.include?("rails (") }
 meet {
   shell "rvm use #{ruby_version} do gem install rails --no-rdoc --no-ri"
