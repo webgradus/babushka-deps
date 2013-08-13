274661d3548b3fff47c98981c254d2e33df7d6ed
PitOn <garik.piton@gmail.com>
2013-08-13 18:31:34 +0400

    source rvm

--- helpers/rvm.rb
+++ helpers/rvm.rb
@@ -11,7 +11,7 @@ def rvm_installed?
 end
 
 def rvm_run cmd    
-    log_shell("rvm_run: #{cmd}", rvm_script + " " + cmd)    
+    log_shell("rvm_run: #{cmd}", "source /usr/local/rvm/scripts/rvm; " + rvm_script + " " + cmd)    
 end
 
 def current_rubies
