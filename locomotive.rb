ee3dfcc2df22af6774f43ba8f241940dd88dfc05
PitOn <garik.piton@gmail.com>
2013-08-13 18:04:44 +0400

    using rvm_run

--- locomotive.rb
+++ locomotive.rb
@@ -1,3 +1,4 @@
+# use this deop ONLY on server cause it uses rvm_run
 dep 'locomotive.local', :host, :app_name do
   requires 'rvm', 'rails installed'.with("1.9.3")
   met? { "/opt/#{app_name}".p.exists? }
@@ -5,7 +6,7 @@ dep 'locomotive.local', :host, :app_name do
   meet {
     cd "/opt" do
       source_rvm
-      shell "rvm use 1.9.3 do rails _3.2.13_ new #{app_name} --skip-active-record --skip-test-unit --skip-javascript --skip-bundle"
+      rvm_run "use 1.9.3 do rails _3.2.13_ new #{app_name} --skip-active-record --skip-test-unit --skip-javascript --skip-bundle"
       cd "/opt/#{app_name}" do
         shell "echo 'rvm_trust_rvmrcs_flag=1; rvm use 1.9.3' > .rvmrc"
         shell %{echo 'gem "locomotive_cms", "~> 2.2.1", :require => "locomotive/engine"' >> Gemfile}
@@ -13,14 +14,14 @@ dep 'locomotive.local', :host, :app_name do
         shell %{echo 'gem "compass-rails", "~> 1.0.2", :group => "assets"' >> Gemfile}
         shell %{echo 'gem "therubyracer", ">= 0.8.2"' >> Gemfile}
         log "bundle install..."
-        shell "rvm use 1.9.3 do bundle install"
+        rvm_run "use 1.9.3 do bundle install"
         log "running locomotive generator..."
-        shell "rvm use 1.9.3 do bundle exec rails g locomotive:install"
+        rvm_run "use 1.9.3 do bundle exec rails g locomotive:install"
         render_erb "locomotive/locomotive.rb.erb", :to => "/opt/#{app_name}/config/initializers/locomotive.rb", :sudo => true
         render_erb "locomotive/mongoid.yml.erb", :to => "/opt/#{app_name}/config/mongoid.yml", :sudo => true
         render_erb "locomotive/carrierwave.rb.erb", :to => "/opt/#{app_name}/config/initializers/carrierwave.rb", :sudo => true
         log "precompiling assets..."
-        shell "rvm use 1.9.3 do bundle exec rake assets:precompile"
+        rvm_run "use 1.9.3 do bundle exec rake assets:precompile"
       end
     end
   }
