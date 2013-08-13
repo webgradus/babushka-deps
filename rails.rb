ad2b68b353dea774d92cf743b29b15ac496c9489
PitOn <garik.piton@gmail.com>
2013-08-13 17:07:08 +0400

    correct rails gem searching

--- rails.rb
+++ rails.rb
@@ -1,5 +1,5 @@
 dep 'rails.installed', :ruby_version do
-met? { raw_shell("rvm use #{ruby_version} do gem list rails").stdout.include?("rails") }
+met? { raw_shell("rvm use #{ruby_version} do gem list rails").stdout.include?("rails (") }
 meet {
   shell "rvm use #{ruby_version} do gem install rails --no-rdoc --no-ri"
 }   
