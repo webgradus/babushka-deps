53bc026ecd4da9a6dceec960e0d13e00dd6f80de
GarPit <garik.piton@gmail.com>
2013-08-16 16:46:33 +0400

    pass block from rvm_run_with_ruby to log_shell

--- helpers/rvm.rb
+++ helpers/rvm.rb
@@ -10,8 +10,8 @@ def rvm_run cmd
     log_shell("rvm_run: #{cmd}", rvm_script + " " + cmd)
 end
 
-def rvm_run_with_ruby ruby_version, cmd
-    log_shell("rvm_run: #{cmd}", rvm_script + " use #{ruby_version} do " + cmd)
+def rvm_run_with_ruby ruby_version, cmd, &block
+    log_shell("rvm_run: #{cmd}", rvm_script + " use #{ruby_version} do " + cmd, &block)
 end
 
 def current_rubies
