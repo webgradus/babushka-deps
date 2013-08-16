c37a77b889f904d9bf34a15a14d7f28641cbfb76
GarPit <garik.piton@gmail.com>
2013-08-16 16:31:29 +0400

    fix rails.installed dep

--- rails.rb
+++ rails.rb
@@ -1,8 +1,8 @@
 dep 'rails installed', :ruby_version do
-met? { 
-  rvm_run("use #{ruby_version} do gem list rails") {|shell| shell.stdout.include?("rails (") }
+met? {
+  rvm_run_with_ruby(ruby_version, "gem list rails") {|shell| shell.stdout.include?("rails (") }
 }
 meet {
-  rvm_run "use #{ruby_version} do gem install rails --no-rdoc --no-ri"
-}   
+  rvm_run_with_ruby ruby_version, "gem install rails --no-rdoc --no-ri"
+}
 end
