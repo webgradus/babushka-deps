18da106fdf32aa6afa2498ebdff78da99e58053f
GarPit <garik.piton@gmail.com>
2013-08-16 17:49:32 +0400

    all scripts should be in meet block

--- backup-server.rb
+++ backup-server.rb
@@ -1,20 +1,20 @@
 dep 'backup-server' do
-    log "backup gem install..."
-    shell "gem install backup"
-    shell "backup generate:config"
-    shell "mkdir models", :cd => "/root/Backup/"
-
-    log "whenever gem install..."
-    shell "gem install whenever"
-    shell "mkdir config", :cd => "/root/Backup/"
-    shell "wheneverize .", :cd => "/root/Backup/"
-
-    log "copy config file..."
     met? {
         Babushka::Renderable.new("/root/Backup/config.rb").from?(dependency.load_path.parent / "backup-server/config.rb.erb")
     }
     meet {
+        log "backup gem install..."
+        shell "gem install backup"
+        shell "backup generate:config"
+        shell "mkdir models", :cd => "/root/Backup/"
+
+        log "whenever gem install..."
+        shell "gem install whenever"
+        shell "mkdir config", :cd => "/root/Backup/"
+        shell "wheneverize .", :cd => "/root/Backup/"
+
+        log "copy config file..."
         render_erb "backup-server/config.rb.erb", :to => ("/root/Backup/config.rb").to_s
     }
-    
+
 end
