9a6976eb48ef228e6183f5291a1e970e4025455c
Alex Nigomatulin <alex4578@yandex.ru>
2013-08-26 09:54:08 +0400

    Update backup-server.rb

--- backup-server.rb
+++ backup-server.rb
@@ -1,20 +1,20 @@
 dep 'backup-server' do
     met? {
-        Babushka::Renderable.new("/root/Backup/config.rb").from?(dependency.load_path.parent / "backup-server/config.rb.erb")
+        Babushka::Renderable.new("~/Backup/config.rb").from?(dependency.load_path.parent / "backup-server/config.rb.erb")
     }
     meet {
         log "backup gem install..."
         shell "gem install backup"
         shell "backup generate:config"
-        shell "mkdir models", :cd => "/root/Backup/"
+        shell "mkdir models", :cd => "~/Backup/"
 
         log "whenever gem install..."
         shell "gem install whenever"
-        shell "mkdir config", :cd => "/root/Backup/"
-        shell "wheneverize .", :cd => "/root/Backup/"
+        shell "mkdir config", :cd => "~/Backup/"
+        shell "wheneverize .", :cd => "~/Backup/"
 
         log "copy config file..."
-        render_erb "backup-server/config.rb.erb", :to => ("/root/Backup/config.rb").to_s
+        render_erb "backup-server/config.rb.erb", :to => ("~/Backup/config.rb").to_s
     }
 
 end
