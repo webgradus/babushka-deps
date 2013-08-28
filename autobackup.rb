0e720c9626d59bc41f1dd8510aa22388b511b781
Alexandr Nigomatulin <nigomatulin@gmail.com>
2013-08-28 13:03:25 +0400

    share database and archives

--- autobackup.rb
+++ autobackup.rb
@@ -6,11 +6,13 @@ dep 'autobackup', :app_name, :app_path, :database, :ruby_version_for_backup_scri
   ruby_version_for_backup_script.default("2.0.0").choose(%w[2.0.0 1.9.3])
 
   met? {
-    Babushka::Renderable.new("~/Backup/models/" + app_name + ".rb").from?(dependency.load_path.parent / "autobackup/model_template.rb.erb")
+    Babushka::Renderable.new("~/Backup/models/" + app_name + "_database.rb").from?(dependency.load_path.parent / "autobackup/model_database_template.rb.erb")&&Babushka::Renderable.new("~/Backup/models/" + app_name + "_archives.rb").from?(dependency.load_path.parent / "autobackup/model_archive_template.rb.erb")
   }
   meet {
-    render_erb "autobackup/model_template.rb.erb", :to => ("~/Backup/models/" + app_name + ".rb").to_s
-    rvm_run_with_ruby ruby_version_for_backup_script, "backup perform -t #{app_name}"
+    render_erb "autobackup/model_database_template.rb.erb", :to => ("~/Backup/models/" + app_name + "_database.rb").to_s
+    rvm_run_with_ruby ruby_version_for_backup_script, "backup perform -t #{app_name}_database"
+    render_erb "autobackup/model_archive_template.rb.erb", :to => ("~/Backup/models/" + app_name + "_archives.rb").to_s
+    rvm_run_with_ruby ruby_version_for_backup_script, "backup perform -t #{app_name}_archives"
   }
   requires "schedule".with(app_name, ruby_version_for_backup_script)
 
@@ -23,8 +25,13 @@ dep "schedule", :app_name, :ruby_version_for_backup_script do
   }
   meet {
     shell %{ echo 'every 1.week do' >> ~/Backup/config/schedule.rb }
-    shell %{ echo '  command "rvm use #{ruby_version_for_backup_script} do backup perform -t #{app_name}"' >> ~/Backup/config/schedule.rb }
+    shell %{ echo '  command "rvm use #{ruby_version_for_backup_script} do backup perform -t #{app_name}_database"' >> ~/Backup/config/schedule.rb }
     shell %{ echo 'end' >> ~/Backup/config/schedule.rb }
+
+    shell %{ echo 'every 1.month do' >> ~/Backup/config/schedule.rb }
+    shell %{ echo '  command "rvm use #{ruby_version_for_backup_script} do backup perform -t #{app_name}_archives"' >> ~/Backup/config/schedule.rb }
+    shell %{ echo 'end' >> ~/Backup/config/schedule.rb }
+
     cd '~/Backup/' do
       rvm_run_with_ruby ruby_version_for_backup_script, "whenever"
       rvm_run_with_ruby ruby_version_for_backup_script, "whenever --update-crontab"
