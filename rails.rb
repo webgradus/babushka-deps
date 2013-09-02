6a9f83198bfa6107e3e053278828ccd7716ee630
PitOn <garik.piton@gmail.com>
2013-09-02 22:52:15 +0400

    check only rails version

--- rails.rb
+++ rails.rb
@@ -1,6 +1,6 @@
 dep 'rails installed', :ruby_version, :rails_version do
 met? {
-  rvm_run_with_ruby(ruby_version, "gem list rails") {|shell| shell.stdout.include?("rails (#{rails_version})") }
+  rvm_run_with_ruby(ruby_version, "gem list rails") {|shell| shell.stdout.include?(rails_version) }
 }
 meet {
   rvm_run_with_ruby ruby_version, "gem install rails -v=#{rails_version} --no-rdoc --no-ri"
