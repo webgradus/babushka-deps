05d129b69c64171f6e0093aa1d35929db5200dfb
PitOn <garik.piton@gmail.com>
2014-02-18 12:57:42 +0300

    remove mysql from default stack

--- stack.rb
+++ stack.rb
@@ -1,3 +1,3 @@
 dep 'stack' do
-  requires 'running.nginx', 'mysql configured', 'postgres.managed', 'latest mongo', 'imagemagick.managed', 'redis', 'rvm'
+  requires 'running.nginx', 'postgres.managed', 'latest mongo', 'imagemagick.managed', 'redis', 'rvm'
 end
