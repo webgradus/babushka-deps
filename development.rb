ac3d22987a9d545229154f3bafcccd9e3cb91815
PitOn <garik.piton@gmail.com>
2015-03-16 14:11:32 +0300

    add and pass web_server param to foreman dep

--- development.rb
+++ development.rb
@@ -1,4 +1,4 @@
-dep 'prepare-deploy', :app_name, :git_username, :server_ip, :app_path, :app_type, :ruby_version, :use_faye do
+dep 'prepare-deploy', :app_name, :git_username, :server_ip, :app_path, :app_type, :ruby_version, :use_faye, :web_server do
   app_name.ask("What is the name of application that will be located at /opt")
   git_username.default!("") #ask('What is your Gitlab username (login) on GitEnterprise')
   server_ip.ask('Where to deploy - I need IP')
@@ -6,15 +6,23 @@ dep 'prepare-deploy', :app_name, :git_username, :server_ip, :app_path, :app_type
   app_path.default!("")
   ruby_version.default("2.1.0")
   use_faye.default('no').choose(%w[yes no])
-  requires 'foreman'.with(app_path, use_faye)
+  web_server.default('unicorn').choose(%w[unicorn puma])
+  requires 'foreman'.with(app_path, use_faye, web_server)
   met? {
     Babushka::Renderable.new(app_path / "config/deploy.rb").from?(dependency.load_path.parent / "development/deploy.rb.erb") && 
-    Babushka::Renderable.new(app_path / "config/unicorn.rb").from?(dependency.load_path.parent / "development/unicorn.rb.erb") &&
-    Babushka::Renderable.new(app_path / "config/deploy/production.rb").from?(dependency.load_path.parent / "development/deploy/production.rb.erb")
+    Babushka::Renderable.new(app_path / "config/deploy/production.rb").from?(dependency.load_path.parent / "development/deploy/production.rb.erb") &&
+    (web_server == "unicorn"
+      ? Babushka::Renderable.new(app_path / "config/unicorn.rb").from?(dependency.load_path.parent / "development/unicorn.rb.erb")
+      : Babushka::Renderable.new(app_path / "config/puma.rb").from?(dependency.load_path.parent / "development/puma.rb.erb")
+    )
   }
   meet {
     render_erb "development/deploy.rb.erb", :to => (app_path / "config/deploy.rb").to_s
     render_erb "development/deploy/production.rb.erb", :to => (app_path / "config/deploy/production.rb").to_s
-    render_erb "development/unicorn.rb.erb", :to => (app_path / "config/unicorn.rb").to_s
+    if web_server == "unicorn"
+      render_erb "development/unicorn.rb.erb", :to => (app_path / "config/unicorn.rb").to_s
+    else
+      render_erb "development/puma.rb.erb", :to => (app_path / "config/puma.rb").to_s
+    end
   }
 end
