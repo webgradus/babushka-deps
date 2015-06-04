cf12f8b0c62e522c94675546ac0a53f284a95d0b
buurzx <oooradius@gmail.com>
2015-06-04 20:00:37 +0500

    Update locomotive.rb

--- locomotive.rb
+++ locomotive.rb
@@ -42,9 +42,9 @@ dep 'locomotive', :host, :app_name, :port do
       log "generating web server config..."
       remote_babushka "webgradus:prepare-deploy", :app_name => app_name, :git_username => "gradus", :server_ip => host, :app_path => "/opt/#{app_name}", :app_type => "locomotive"#, :web_server => "puma"
       log "exporting init script..."
-      remote_babushka "webgradus:foreman.export", :app_path => "/opt/#{app_name}", :use_faye => "no", :web_server => web_server
+      remote_babushka "webgradus:foreman.export", :app_path => "/opt/#{app_name}", :use_faye => "no", :web_server => "unicorn"
       log "starting Web server..."
-      remote_babushka "webgradus:foreman.start", :app_path => "/opt/#{app_name}", :use_faye => "no", :web_server => web_server
+      remote_babushka "webgradus:foreman.start", :app_path => "/opt/#{app_name}", :use_faye => "no", :web_server => "unicorn"
       log "generating Nginx server - restarting Nginx..."
       remote_babushka "webgradus:unicorn-server", :app_name => app_name, :port => port, :app_type => "locomotive"
     }
