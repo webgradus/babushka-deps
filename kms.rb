fabe8314ec7c4118beacae41c0b4cca8eca7edfe
GarPit <garik.piton@gmail.com>
2015-11-13 18:42:21 +0300

    trying not to use rvm_run when we have .ruby-version

--- kms.rb
+++ kms.rb
@@ -16,24 +16,24 @@ dep 'kms installed', :app_name, :ruby_version, :postgres_password do
   meet {
     cd "/opt" do
       rvm_run_with_ruby ruby_version, "rails _4.2.5_ new #{app_name} --skip-test-unit --skip-javascript --skip-bundle --database=postgresql"
+      shell "echo '#{ruby_version}' > .ruby-version", cd: app_name
       cd "#{app_name}", create: true do
-        shell "echo '#{ruby_version}' > .ruby-version"
         shell %{echo 'gem "kms", git: "git@gitlab.com:webgradus/kms.git"' >> Gemfile}
         shell %{echo 'gem "puma"' >> Gemfile}
         shell %{echo 'gem "sprockets", "2.12.4"' >> Gemfile}
         shell %{mkdir tmp/pids; mkdir tmp/sockets}
         log "bundle install..."
-        rvm_run_with_ruby ruby_version, "bundle install"
+        shell %{bundle install}
         log "setup database.yml..."
         render_erb "kms/database.yml.erb", to: "config/database.yml"
         log "running kms generator..."
-        rvm_run_with_ruby ruby_version, "RAILS_ENV=production bundle exec rails g kms:install"
+        shell %{RAILS_ENV=production bundle exec rails g kms:install}
         log "install migrations..."
-        rvm_run_with_ruby ruby_version, "RAILS_ENV=production bundle exec rake kms:install:migrations"
+        shell %{RAILS_ENV=production bundle exec rake kms:install:migrations}
         log "applying migrations..."
-        rvm_run_with_ruby ruby_version, "RAILS_ENV=production bundle exec rake db:migrate"
+        shell %{RAILS_ENV=production bundle exec rake db:migrate}
         log "precompiling assets..."
-        rvm_run_with_ruby ruby_version, "RAILS_ENV=production bundle exec rake assets:precompile"
+        shell %{RAILS_ENV=production bundle exec rake assets:precompile}
       end
     end
   }
