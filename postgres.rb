58639073f52df09a7c7480c2a6b0cb47745fe3fe
PitOn <garik.piton@gmail.com>
2016-01-22 09:49:58 +0300

    Update postgres dependency

--- postgres.rb
+++ postgres.rb
@@ -58,7 +58,7 @@ dep 'postgres.apt_repository' do
 end
 
 dep 'postgres.managed', :version do
-  version.default('9.4')
+  version.default('9.5')
   # Assume the installed version if there is one
   #version.default!(shell('psql --version').val_for('psql (PostgreSQL)')[/^\d\.\d/]) if which('psql')
   requires 'common:set.locale'.with("en_US")
