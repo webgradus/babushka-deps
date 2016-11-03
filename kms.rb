3dc710db413f52d605a78a2f794587bee996806e
Igor Petrov <garik.piton@gmail.com>
2016-11-03 10:06:23 +0300

    Rails 5 by default for new KMS

--- kms.rb
+++ kms.rb
@@ -10,14 +10,14 @@ end
 
 dep 'kms installed', :app_name, :ruby_version, :postgres_password do
   # check if we have rails and if we have access to KMS repo
-  requires 'rails installed'.with(ruby_version, "4.2.7.1"), 'repo accessible'.with("git@gitlab.com:webgradus/kms.git")
+  requires 'rails installed'.with(ruby_version, "5.0.0.1"), 'repo accessible'.with("git@gitlab.com:webgradus/kms.git")
   postgres_password.ask("Please type PostgreSQL password for user 'postgres'")
 
   met? { "/opt/#{app_name}".p.exists? }
 
   meet {
     cd "/opt" do
-      rvm_run_with_ruby ruby_version, "rails _4.2.7.1_ new #{app_name} --skip-test-unit --skip-bundle --database=postgresql"
+      rvm_run_with_ruby ruby_version, "rails _5.0.0.1_ new #{app_name} --skip-test-unit --skip-bundle --database=postgresql"
       shell "echo '#{ruby_version}' > .ruby-version", cd: app_name
       cd "#{app_name}", create: true do
         shell %{echo 'gem "kms"' >> Gemfile}
@@ -33,19 +33,19 @@ dep 'kms installed', :app_name, :ruby_version, :postgres_password do
         log "setup locale..."
         raw_shell %{echo "I18n.default_locale = :ru" >> config/application.rb}
         log "creating database..."
-        rvm_shell %{RAILS_ENV=production bundle exec rake db:create}
+        rvm_shell %{RAILS_ENV=production bundle exec rails db:create}
         log "running kms generator..."
         rvm_shell %{RAILS_ENV=production bundle exec rails g kms:install}
         log "running kms_models generator..."
         rvm_shell %{RAILS_ENV=production bundle exec rails g kms_models:install}        
         log "install migrations..."
-        rvm_shell %{RAILS_ENV=production bundle exec rake kms:install:migrations}
+        rvm_shell %{RAILS_ENV=production bundle exec rails kms:install:migrations}
         log "install kms_models migrations..."
-        rvm_shell %{RAILS_ENV=production bundle exec rake kms_models:install:migrations}
+        rvm_shell %{RAILS_ENV=production bundle exec rails kms_models:install:migrations}
         log "applying migrations..."
-        rvm_shell %{RAILS_ENV=production bundle exec rake db:migrate}
+        rvm_shell %{RAILS_ENV=production bundle exec rails db:migrate}
         log "precompiling assets..."
-        rvm_shell %{RAILS_ENV=production bundle exec rake assets:precompile}
+        rvm_shell %{RAILS_ENV=production bundle exec rails assets:precompile}
       end
     end
   }
