e97abfb378da9e2a9c0a13adcad5278adc08d418
Igor Petrov <garik.piton@gmail.com>
2017-02-15 12:12:54 +0300

    Add light-stack dep

--- stack.rb
+++ stack.rb
@@ -1,3 +1,6 @@
 dep 'stack' do
   requires 'running.nginx', 'postgres.managed', 'imagemagick.managed', 'redis', 'rvm', 'eye.running'
 end
+dep 'light-stack' do
+  requires 'running.nginx', 'postgres.managed', 'imagemagick.managed', 'rvm', 'eye.running'
+end
