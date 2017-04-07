5e6eed43687f4118ff37986593e5dd396b86dbf8
GarPit <garik.piton@gmail.com>
2017-04-07 10:34:37 +0300

    fix linking eye executable"

--- eye.rb
+++ eye.rb
@@ -27,7 +27,7 @@ dep 'eye.installed' do
     eye_lib_path = rvm_run_with_ruby "2.3.3", "gem which eye"
     #puts eye_lib_path
     #puts File.expand_path(eye_lib_path, "../bin/eye")
-    shell("ln -sf #{File.expand_path(eye_lib_path, "../bin/eye")} /usr/local/bin/eye")
+    shell("ln -sf #{File.expand_path(File.join(eye_lib_path, "../../bin/eye"))} /usr/local/bin/eye")
     shell("mkdir /root/eye")
   }
 end
