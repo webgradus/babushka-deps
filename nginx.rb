80a5f93b1a3c1aab06a4b527e16aad625eaf777c
Igor Petrov <garik.piton@gmail.com>
2017-05-22 10:25:48 +0300

    update nginx version

--- nginx.rb
+++ nginx.rb
@@ -154,7 +154,7 @@ end
 
 dep 'nginx.src', :nginx_prefix, :version, :upload_module_version do
   nginx_prefix.default!("/opt/nginx")
-  version.default!('1.10.3')
+  version.default!('1.12.0')
   upload_module_version.default!('2.2')
 
   requires 'pcre.lib', 'libssl.lib', 'zlib.lib'
